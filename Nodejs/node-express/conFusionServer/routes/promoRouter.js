d7da4558b8172da0d1b476f7547159648c736c6a
jhonny <anjandudda@gmail.com>
2020-07-14 12:24:03 +0530

    Express Generator REST API

---
+++ Nodejs/node-express/conFusionServer/routes/promoRouter.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const promoRouter = express.Router();
+
+promoRouter.use(bodyParser.json());
+
+promoRouter.route('/')
+.all((req,res,next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+})
+.get((req,res,next) => {
+    res.end('Will send all the prmotions to you!');
+})
+.post((req, res, next) => {
+    res.end('Will add the promotion: ' + req.body.name + ' with details: ' + req.body.description);
+})
+.put((req, res, next) => {
+    res.statusCode = 403;
+    res.end('PUT operation not supported on /promotion');
+})
+.delete((req, res, next) => {
+    res.end('Deleting all promotions');
+});
+
+promoRouter.route('/:promoId')
+.all((req,res,next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+})
+.get((req, res, next) => {
+    res.end('Get the promotions ' + req.params.promoId)
+})
+.post((req, res, next) => {
+    res.end('Will add the promotion id : ' + req.params.promoId + ' with details: ' + req.body.description);
+})
+.put((req, res, next) => {
+    res.statusCode = 403;
+    res.end('PUT operation not supported on /promotion');
+})
+.delete((req, res, next) => {
+    res.end('Deleting promotion with id: ' +  req.params.promoId);
+});
+
+module.exports = promoRouter;
